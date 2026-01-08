db64718d31301ee060b9ade70ca715f665b77eb5
Alex Gillon <alexandergillon@outlook.com>
2026-01-07 22:10:03 -0600

    Adds skeleton Vue app.

---
+++ vue/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Lorem Ipsum</h1>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin convallis urna vel felis bibendum euismod. 
+    Duis gravida molestie magna, ac vehicula mauris tempus eu. Mauris egestas quam eget neque consequat sodales. 
+    Sed nec tempus risus. Integer vitae blandit dolor. Vivamus ut sem congue, sodales quam eu, pellentesque ligula. 
+    Pellentesque porttitor augue ut nisi consectetur, non iaculis quam convallis. Mauris nec eleifend massa. 
+    Phasellus at nisl vitae enim viverra elementum quis at nisl. Mauris vulputate imperdiet odio sed ultricies. 
+    Vestibulum semper leo ultricies justo malesuada, id interdum mauris elementum. Sed eu blandit eros, nec 
+    hendrerit erat. Proin efficitur, magna ac euismod hendrerit, nisl metus cursus ex, at feugiat magna lacus 
+    nec dui. Donec turpis mauris, ornare in lectus vitae, accumsan facilisis elit. Sed lacinia consequat tincidunt.
+  </p>
+</template>
+
+<script setup lang="ts"></script>
---
+++ vue/vite.config.ts
@@ -0,0 +1,24 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: '../docs',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'photography.js',
+      }
+    }
+  },
+})
